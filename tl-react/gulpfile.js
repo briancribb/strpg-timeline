6274b31edc2362d92512f5d6f5efb6ec81e75d0d
Brian Cribb <brian@briancribb.com>
2016-10-04 06:53:43 -0400

    New React version.
    Setting up for jQuery and React versions with common vendor files.

---
+++ tl-react/gulpfile.js
@@ -0,0 +1,139 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cleanCSS = require('gulp-clean-css'),
+	//gutil = require('gulp-util'),
+	rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+	return gulp.src('css/styles.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('css'))
+		.pipe(rename('style.min.css'))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+	return gulp
+		.src([
+			'js/scripts.js'
+		])
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('js'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify())
+		//.pipe(uglify().on('error', gutil.log))
+		.pipe(gulp.dest('js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+	//gulp.watch('js/theme/*.js', ['lint', 'scripts']);
+	gulp.watch('js/scripts.js', ['scripts']);
+	gulp.watch('css/_partials/*.scss', ['sass']);
+	gulp.watch('css/styles.scss', ['sass']);
+});
+
+// Default Task
+//gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'watch']);
+
+
+/*
+JavaScript module tree, to show dependencies.
+
+- AE.js
+	- AE.account.js
+	- AE.booking.js
+		- AE.booking.savequote.js
+		- AE.booking.terms.js
+		- AE.booking.matrix.js
+			- AE.booking.matrix.results.js
+		- AE.booking.optequip.js
+	- AE.cookie.js
+	- AE.forms.js
+	- AE.ga.js
+	- AE.init.js
+	- AE.legacy.js
+	- AE.maps.js
+	- AE.mura.js
+	- AE.nav.js
+	- AE.newsletter.js
+	- AE.template.js
+	- AE.ui.js
+		- AE.ui.datepicker.js
+		- AE.ui.tabcordion.js
+		- AE.ui.dialog.js
+		- AE.ui.modify.js
+		- AE.ui.autocomplete.js
+		- AE.ui.selectmenu.js
+	- AE.utils.js
+
+
+
+=====================
+Setting up Gulp
+=====================
+
+
+There are two files that will be version controlled for Gulp. They are: 
+package.json	: A general description of this project. It must be present, but we can update as needed. 
+gulpfile.js		: This file that you're reading! It has all the instructions for what Gulp is going to do for us.
+
+
+To get started, Node must be installed. There's an installer on the official site.
+https://nodejs.org/en/
+
+
+Here's an excellent article that will walk you through setting up Gulp:
+https://travismaynard.com/writing/getting-started-with-gulp
+
+
+There's a "gotcha" in that article, however. The "gulp-jshint" plugin has a dependence of "jshint". So the big install 
+command that Travis Maynard's article gives you needs to include "jshint" in front of "gulp-jshint". It also needs to 
+include a way to minify the CSS since the gulp-uglify plugin only handles JavaScript. Here's the completed command: 
+
+npm install jshint gulp-jshint gulp-sass gulp-concat gulp-uglify clean-css gulp-clean-css gulp-rename gulp-livereload --save-dev
+
+Here's an explanation on why that is:
+https://github.com/olefredrik/FoundationPress/issues/664
+
+
+
+=====================
+Watching Files: 
+=====================
+If you include the 'watch' task, then Gulp will keep running in the terminal. When a file in a watched folder gets 
+saved, the 'watch' task will run. This will keep running until you enter Control+C to stop it. If you don't want to 
+run it that way, then you could just type "gulp" when you want to update the files. We've defined a default task, so 
+we don't have to name it. (Although we could enter "gulp default" if we want to.)
+
+
+
+* Probably don't need this part, but just in case: *
+=======================================================================================================================
+I didn't have any permission problems, but if you do then this might help: 
+https://docs.npmjs.com/getting-started/fixing-npm-permissions
+
+But it also leaves something out. The video expects you to edit a text file in the terminal but doesn't say what 
+keyboard commands were needed to get this done. Here's a reference for that: 
+http://stackoverflow.com/questions/37365179/open-a-file-in-editor-and-then-save-it-back-by-terminal-on-bash
+
+
+
+*/
---
+++ tl-react/js/all.js
@@ -0,0 +1,450 @@
+/*
+===================================================================================================
+This module declares top-level methods and instantiates the AE object. It must be loaded first.
+===================================================================================================
+*/
+
+
+/*
+The TL object will be the returned result of a self-invoked function. This allows us to expose methods 
+for use by other modules while protecting some variables in a closure. If the APP.ui object is already 
+defined then it's not altered. This prevents us from running the entire function each time one of its 
+internal methods are called.
+*/
+;var TL = TL || (function(){
+	/* This is where we set variables and things that will only live inside the closure. */
+
+
+	/*
+	The id value is to identify the worksheets within the Google Sheet that this data came from. Since the data is unlikely to 
+	change, I just put the current response into some json files and called them locally.
+	*/ 
+	var dfd_array = [],
+		dfd_sources = {
+			UFP: { path:'js/data/UFP.json', id: 'oesera6', name: 'United Federation of Planets' },
+			KLE: { path:'js/data/KLE.json', id: 'o15noc3', name: 'Klingon Empire' },
+			RSA: { path:'js/data/RSA.json', id: 'or7u0kt', name: 'Romulan Star Empire' },
+			TRI: { path:'js/data/TRI.json', id: 'ol08r1n', name: 'Triangle' },
+			ORC: { path:'js/data/ORC.json', id: 'ohu3d91', name: 'Orion Colonies' },
+			RFW: { path:'js/data/RFW.json', id: 'oypmvfb', name: 'Romulan/Federation War' },
+			FYW: { path:'js/data/FYW.json', id: 'oi1ju2s', name: 'Four Years War' },
+			ST3: { path:'js/data/ST3.json', id: 'o5oxeec', name: 'Star Trek 3 Update' },
+			ST4: { path:'js/data/ST4.json', id: 'oxxpvso', name: 'Star Trek 4 Update' },
+			SFI: { path:'js/data/SFI.json', id: 'ohqn30t', name: 'Starfleet Intelligence' },
+			ITA: { path:'js/data/ITA.json', id: 'orojt89', name: "UFP/Independent Traders' Association" }
+		};
+
+
+
+
+	var APP = {
+		resizeTasks : [],
+		events : [],
+		data : {},
+		init : function() {
+			APP.props = {
+				$bodyElement		: $('body'),
+				$navbar				: $('#navbar'),
+				$inputCentury		: $('#century-search'),
+				$sourceToggles		: $('#tl-source'),
+				$sourceToolbar		: $('#source-toolbar'),
+				$timeline			: $('#timeline'),
+				$pageHeader			: $('#page-header'),
+				$pageFooter			: $('#page-footer'),
+				$pageFooterContent	: $('#page-footer-content')
+			};
+
+			/*
+			Setup throttling for resize tasks. Run the manageResize function once upon init. This part relies upon having UnderscoreJS 
+			loaded along with jQuery. It runs all resize tasks every quarter-second.
+			http://underscorejs.org/
+			*/
+			var throttled = _.throttle(APP.manageResize, 250);
+			$(window).resize(throttled);
+			APP.manageResize();
+
+
+			/*
+			Initial resize tasks. For this small project, all tasks are in here, but for larger projects we could have additional tasks 
+			added by other modules.
+			 */
+			var initTasks = {
+				func: function(){
+
+					/* Correct footer height upon resize */
+					var footerHeight = APP.props.$pageFooterContent.outerHeight(true);
+					APP.props.$pageFooter.height( footerHeight );
+					APP.props.$bodyElement.css( 'padding-bottom', footerHeight );
+
+					var siteSize = APP.getSiteViewType();
+
+					/* Correct top body padding for navbar height */
+					APP.props.$bodyElement.css( 'padding-top', APP.props.$navbar.outerHeight(true) );
+
+
+					// Support for CSS break-points
+					if ( siteSize === 'desk' || siteSize === 'deskWide' ) {
+						// Add or remove classes if the window is big.
+					} else {
+						// Add or remove classesif the window is small.
+					}
+				},
+				args:[] // No arguments, so it's an empty array.
+			};
+			initTasks.func(initTasks.args); // Initial call of initial resize task.
+			APP.addResizeTask(initTasks);
+
+
+			APP.getData();
+			APP.addListeners();
+		},
+		addResizeTask : function(task) {
+			/*
+			Adds and object with a "func" property and an "args" property. This is VERY important, because we use the apply() method 
+			in manageResize(). All tasks in the queue will be run during resize. We throttle resizing to keep things from getting too crazy.
+
+			Basic syntax:
+			myTask = {func:myfunction, args:[arg1,arg2]}
+
+			Example of adding a task:
+			var myTask = { func: function(){console.log("I'm resizing!")} }
+			APP.addResizeTask(myTask);
+
+			Or you could be fancy and do this:
+			APP.addResizeTask( { func: function(){console.log("I'm resizing!")} } );
+			*/
+
+			task.args = task.args || [];
+			APP.resizeTasks.push(task);
+		},
+		getData : function() {
+			/* Using deferred objects to make sure we get everything before proceeding. */
+
+			$.each( dfd_sources, function( key, value ) {
+				/*
+				key:	'UFP'
+				value:	{ path:'my/path.json', id: 'myID', name: 'My Name' }
+				*/
+				value.dfd = $.Deferred();
+				dfd_array.push(value.dfd);
+
+				/*
+				If I wanted to do something when each individual thingy resolves, then I would do that here. This done() 
+				function will fire whenever this deferred object is resolved.
+				
+				value.dfd.done(function() {
+					// Do stuff.
+				});
+
+				*/
+
+				$.ajax({
+					url: value.path,
+					dataType: "json"
+				}).success(function(data) {
+
+					/* The data structure is straight from Google, so we still need to drill down into it to get our array. */
+					APP.data[key] = data.feed.entry;
+
+					/* Process data into the main timeline. */
+					for (var i = 0; i < data.feed.entry.length; i++) {
+
+						var dateParts = starToDate( data.feed.entry[i].gsx$stardate.$t );
+
+						APP.events.push({
+							stardate	:	data.feed.entry[i].gsx$stardate.$t,
+							sortkey		:	Number(dateParts.year + '.' + dateParts.month + '' + dateParts.date),
+							end			:	data.feed.entry[i].gsx$stardateend.$t || null,
+							century		:	dateParts.century,
+							year		:	dateParts.year,
+							month		:	dateParts.month,
+							date		:	dateParts.date,
+							source		:	data.feed.title.$t,
+							full		:	(data.feed.entry[i].gsx$full.$t === "TRUE"),
+							desc		:	data.feed.entry[i].gsx$event.$t
+						});
+					};
+
+					/* All done with this JSON file, so we'll resolve its Deferred object. */
+					value.dfd.resolve();
+				});
+			});
+			/* http://stackoverflow.com/questions/5627284/pass-in-an-array-of-deferreds-to-when */
+			$.when.apply(null, dfd_array).done(function() {
+				//console.log("All of the ajax calls are complete. Length is " + APP.events.length);
+				APP.events = _.sortBy(APP.events, 'year');
+				APP.buildTimeline( $('#tl-list') );
+			});
+
+			function starToDate(stardate) {
+				/* format: CC/YYMM.DD */
+				var starSplit, dateSplit, dateParts;
+
+				/* Split the year from the date. */
+				starSplit = stardate.replace(/,/g, '').split('/');
+				dateParts =  {
+					year : 2000 + (Number(starSplit[0]) * 100) + Number(starSplit[1].substring(0,2))
+				}
+
+				if (starSplit[1].length > 2) {
+					dateSplit = starSplit[1].split('.');
+				}
+
+				dateParts.century = starSplit[0];
+				dateParts.month = (dateSplit[0].substring(2,4));
+				dateParts.date = (dateSplit[1] || "00");
+
+				dateParts.month = ( dateParts.month === '00' ) ? '01' : dateParts.month
+				dateParts.date = ( dateParts.date === '00' ) ? '01' : dateParts.date
+				return dateParts;
+			}
+		},
+		buildTimeline : function($target) {
+			var allKeys = _.allKeys(dfd_sources);
+
+			_.each(dfd_sources, function(value, key, list){
+				$target.addClass(key);
+			});
+
+			/*
+			The markup for an event looks like this: 
+			<li>
+				<div class="tl-badge"><i class="glyphicon glyphicon-check"></i></div>
+				<div class="tl-panel">
+					<div class="tl-heading">
+						<h4 class="tl-title">Mussum ipsum cacilds</h4>
+						<p><small class="tl-subtext text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+					</div>
+					<div class="tl-body">
+						<p>Body stuff here.</p>
+					</div>
+				</div>
+			</li>
+			*/
+
+			var documentFragment = $(document.createDocumentFragment());
+
+			for (var i = 0; i < APP.events.length; ++i) {
+
+				APP.events[i].id = i;
+
+				var $li = $("<li/>", { 
+										id: "tl-event-" + i,
+				 						class: "tl-event " + APP.events[i].source + " tl-full-" + APP.events[i].full,
+				 						'data-century': APP.events[i].century
+				 					}),
+					$badge = $("<div/>", { class: "tl-badge" }),
+					$panel = $("<div/>", { class: "tl-panel" }),
+					$heading = $("<div/>", { class: "tl-heading" }),
+					$title = $("<h4/>", { class: "tl-title", text: APP.events[i].stardate }),
+					$body = $("<div/>", { class: "tl-body" });
+
+				/* Putting it all together. */
+				//$badge.append( $("<i/>", { class: "glyphicon glyphicon-check" }) );
+				$heading.append( $title ).append( '<p><small class="tl-subtext text-muted"><i class="glyphicon glyphicon-folder-open"></i>' + dfd_sources[APP.events[i].source].name + '</small></p>' );
+				$body.append( APP.events[i].desc );
+				$panel.append( $heading ).append( $body );
+				$li.append( $badge ).append( $panel );
+
+				documentFragment.append($li);
+			}
+			$target.append(documentFragment);
+
+			/*
+			Remove the "loading" class to reveal the timeline and hide the spinner.
+
+			Webkit browsers don't animate until the page is loaded, resulting in no spin for me because the local 
+			content comes in pretty quick. Firefox spins immediately. This but isn't important enough for me to chase 
+			down right now, but I wanted to make a note of it.
+			*/
+			APP.props.$bodyElement.removeClass('loading');
+			APP.alternateFloats();
+		},
+		addListeners : function() {
+			/* One body listener controls the entire app. */
+			APP.props.$bodyElement.on( "click", function(event) {
+				var $target = $(event.target);
+
+				/* Add or subtract category classes depending upon which button was clicked or scroll to a position. */
+				switch (event.target.id) {
+
+					/* Major Governments */
+					case 'UFP-toggle':
+						toggleCategory('UFP');
+						break;
+					case 'KLE-toggle':
+						toggleCategory('KLE');
+						break;
+					case 'KLE-full':
+						toggleCategory('KLE-full');
+						break;
+
+
+					case 'RSA-toggle':
+						toggleCategory('RSA');
+						break;
+					case 'RSA-full':
+						toggleCategory('RSA-full');
+						break;
+
+
+					case 'ORC-toggle':
+						toggleCategory('ORC');
+						break;
+					case 'ORC-full':
+						toggleCategory('ORC-full');
+						break;
+
+
+					/* Miscellaneous */
+					case 'TRI-toggle':
+						toggleCategory('TRI');
+						break;
+					case 'ST3-toggle':
+						toggleCategory('ST3');
+						break;
+					case 'ST4-toggle':
+						toggleCategory('ST4');
+						break;
+					case 'SFI-toggle':
+						toggleCategory('SFI');
+						break;
+					case 'ITA-toggle':
+						toggleCategory('ITA');
+						break;
+
+
+					/* Wars */
+					case 'RFW-toggle':
+						toggleCategory('RFW');
+						break;
+					case 'FYW-toggle':
+						toggleCategory('FYW');
+						break;
+
+					/* Return to the top */
+					case 'btn-return-top':
+						//window.scroll(0, 0);
+						APP.scrollToPosition(0);
+						break;
+					case 'navbar-brand':
+						//window.scroll(0, 0);
+						window.scrollTo(0, 0);
+						break;
+
+					/* Return to the top */
+					case 'btn-footer':
+						//window.scroll(0, 0);
+						APP.scrollToPosition( APP.props.$pageFooter.position().top );
+						break;
+
+					/* Search for Century */
+					case 'btn-search':
+						scrollToCentury();
+						break;
+
+					default:
+						/* If there's no id or it doesn't match anything, then do nothing. */
+						break;
+				}
+				APP.alternateFloats();
+
+				/* This function must be inside the on() block because it uses the event target's jQuery collection. */
+				function toggleCategory(category) {
+					if ( $target.hasClass('active') ) {
+						$target.removeClass('active');
+						APP.props.$timeline.removeClass( category );
+					} else {
+						$target.addClass('active');
+						APP.props.$timeline.addClass(category);
+					}
+				}
+			});
+
+			APP.props.$inputCentury.on( "keypress", function(event) {
+				if (event.which == '13') {
+					event.preventDefault();
+					scrollToCentury();
+				}
+			});
+
+			function scrollToCentury() {
+
+				var century = APP.props.$inputCentury.val();
+				if ( century !== '' ) {
+					if ( Number(century) === NaN ) {
+						APP.props.$inputCentury.val('');
+					} else {
+						var scrollTarget = null,
+							offset =	APP.props.$pageHeader.outerHeight(true);// + 
+										//APP.props.$sourceToggles.outerHeight(true);
+
+						$('.tl-event:visible').each(function (i) {
+							var currentCentury = $(this).data('century');
+							if ( currentCentury >= century ) {
+								scrollTarget = offset + $(this).position().top;
+								return false;
+							}
+						});
+						if (scrollTarget !== null) {
+							APP.scrollToPosition(scrollTarget);
+							APP.props.$inputCentury.val('');
+						}
+					}
+				}
+			}
+		},
+		scrollToPosition : function(position) {
+			$('html,body').animate( { scrollTop: position }, 1000 );
+		},
+		alternateFloats : function() {
+			/* Clear out the right-floats and reset them on every other visible timeline event. */
+			$('.tl-inverted').removeClass('tl-inverted');
+			$('.tl-event:visible').each(function (i) {
+				/* Test for i+1 because i will start as zero. */
+				if ( (i+1) % 2 === 0) {
+					$(this).addClass('tl-inverted');
+				}
+			});
+		},
+		manageResize : function() {
+			/* Cycle through resize tasks. */
+			for (var i = 0; i < APP.resizeTasks.length; i++) {
+				var task = APP.resizeTasks[i];
+				task.func.apply(this, task.args);
+			};
+		},
+		getSiteViewType : function() {
+			var sizes = {
+				/*
+				Support for CSS break-points, set to match Bootstrap default values. If you change the breakpoints in your 
+				CSS then you need to change them here as well. This function should be used in harmony with CSS, making 
+				whatever adjustments are needed as the window size changes.
+				*/
+					xs		: 480,
+					sm		: 768,
+					md		: 992,
+					lg		: 1200
+				},
+				currentSize = APP.props.$bodyElement.outerWidth(true),
+				sizeType = 'xs';
+
+			if ( currentSize >= sizes.sm ) {
+				sizeType = "sm";
+			}
+			if ( currentSize >= sizes.md ) {
+				sizeType = "md";
+			}
+			if ( currentSize >= sizes.lg ) {
+				sizeType = "lg";
+			}
+			return sizeType;
+		}
+	};	
+	return APP;
+})();
+
+/* All modules loaded, time to kick off the whole thing. */
+$(document).ready(function() {
+	TL.init();
+});

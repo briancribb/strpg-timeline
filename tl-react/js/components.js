54a4560244233266519a398f472a5fb4c312fbfa
Brian Cribb <brian@briancribb.com>
2016-12-12 06:03:06 -0500

    Button behaviors.

--- tl-react/js/components.js
+++ tl-react/js/components.js
@@ -16,25 +16,26 @@ class Timeline extends React.Component {
 		//console.log(this.state);
 	}
 	componentDidUpdate() {
-		if (this.state.sources) {
+		if (this.state.initialized) {
 			this._alternateFloats();
 		}
 	}
+
 	_getData() {
 		var that = this,
 			dfd_array = [],
 			dfd_sources = {
-				UFP: { path:'js/data/UFP.json', id: 'oesera6', hasFull: false, name: 'United Federation of Planets' },
-				KLE: { path:'js/data/KLE.json', id: 'o15noc3', hasFull: true,  name: 'Klingon Empire' },
-				RSA: { path:'js/data/RSA.json', id: 'or7u0kt', hasFull: true,  name: 'Romulan Star Empire' },
-				TRI: { path:'js/data/TRI.json', id: 'ol08r1n', hasFull: false, name: 'Triangle' },
-				ORC: { path:'js/data/ORC.json', id: 'ohu3d91', hasFull: true,  name: 'Orion Colonies' },
-				RFW: { path:'js/data/RFW.json', id: 'oypmvfb', hasFull: false, name: 'Romulan/Federation War' },
-				FYW: { path:'js/data/FYW.json', id: 'oi1ju2s', hasFull: false, name: 'Four Years War' },
-				ST3: { path:'js/data/ST3.json', id: 'o5oxeec', hasFull: false, name: 'Star Trek 3 Update' },
-				ST4: { path:'js/data/ST4.json', id: 'oxxpvso', hasFull: false, name: 'Star Trek 4 Update' },
-				SFI: { path:'js/data/SFI.json', id: 'ohqn30t', hasFull: false, name: 'Starfleet Intelligence' },
-				ITA: { path:'js/data/ITA.json', id: 'orojt89', hasFull: false, name: "UFP/Independent Traders' Association" }
+				UFP: { path:'js/data/UFP.json', id: 'oesera6', hasFull: false, show:true, name: 'United Federation of Planets' },
+				KLE: { path:'js/data/KLE.json', id: 'o15noc3', hasFull: true,  show:true, name: 'Klingon Empire' },
+				RSA: { path:'js/data/RSA.json', id: 'or7u0kt', hasFull: true,  show:true, name: 'Romulan Star Empire' },
+				TRI: { path:'js/data/TRI.json', id: 'ol08r1n', hasFull: false, show:true, name: 'Triangle' },
+				ORC: { path:'js/data/ORC.json', id: 'ohu3d91', hasFull: true,  show:true, name: 'Orion Colonies' },
+				RFW: { path:'js/data/RFW.json', id: 'oypmvfb', hasFull: false, show:true, name: 'Romulan/Federation War' },
+				FYW: { path:'js/data/FYW.json', id: 'oi1ju2s', hasFull: false, show:true, name: 'Four Years War' },
+				ST3: { path:'js/data/ST3.json', id: 'o5oxeec', hasFull: false, show:true, name: 'Star Trek 3 Update' },
+				ST4: { path:'js/data/ST4.json', id: 'oxxpvso', hasFull: false, show:true, name: 'Star Trek 4 Update' },
+				SFI: { path:'js/data/SFI.json', id: 'ohqn30t', hasFull: false, show:true, name: 'Starfleet Intelligence' },
+				ITA: { path:'js/data/ITA.json', id: 'orojt89', hasFull: false, show:true, name: "UFP/Independent Traders' Association" }
 			},
 			objData = {
 				sources:{},
@@ -52,7 +53,7 @@ class Timeline extends React.Component {
 			/*
 			If I wanted to do something when each individual thingy resolves, then I would do that here. This done() 
 			function will fire whenever this deferred object is resolved.
-			
+
 			value.dfd.done(function() {
 				// Do stuff.
 			});
@@ -70,9 +71,10 @@ class Timeline extends React.Component {
 				/* Keep some stuff from the dfd_sources array. */
 				objData.sources[key] = {
 					name: value.name,
-					hasFull : value.hasFull
+					hasFull : value.hasFull,
+					show : value.show,
+					showFull : false
 				}
-
 
 				/* Process data into the main timeline. */
 				for (var i = 0; i < data.feed.entry.length; i++) {
@@ -81,6 +83,7 @@ class Timeline extends React.Component {
 
 					objData.entries.push({
 						show		:	true,
+						inverted	:	false,
 						stardate	:	data.feed.entry[i].gsx$stardate.$t,
 						sortkey		:	Number(dateParts.year + '.' + dateParts.month + '' + dateParts.date),
 						end			:	data.feed.entry[i].gsx$stardateend.$t || null,
@@ -92,7 +95,7 @@ class Timeline extends React.Component {
 						full		:	(data.feed.entry[i].gsx$full.$t === "TRUE"),
 						desc		:	data.feed.entry[i].gsx$event.$t
 					});
-				};
+				};  
 
 				/* All done with this JSON file, so we'll resolve its Deferred object. */
 				value.dfd.resolve();
@@ -105,16 +108,28 @@ class Timeline extends React.Component {
 			objData.entries = _.sortBy(objData.entries, 'year');
 			that.setState(objData);
 
+			that.setState({initialized:true});
+			//that._updateEntries();
 			that._manageLayout();
+
+			that._alternateFloats();
+
+			//that._updateSource('ORC');
+			//that._updateSource('RSA', 'full');
+
+
+
+			/*
+			let newObj = _.clone(that.state.sources);
+			newObj.ORC.show = false;
+			newObj.RSA.showFull = true;
+			that.setState({
+				sources: newObj
+			});
+			*/
+
 			console.log('that.state');
 			console.log(that);
-
-			// Hide anything with a key over 50, as a test.
-			let arrTemp = _.map(that.state.entries, function(entry){
-				entry.show = (entry.key < 50) ? true : false;
-				return entry;
-			});
-			that.setState({entries: arrTemp});
 
 
 		});
@@ -147,7 +162,9 @@ class Timeline extends React.Component {
 		let key = 0;
 		return this.state.entries.map((entry) => {
 			entry.key = key;
-			let markup = <TLEntry obj={entry} name={this.state.sources[entry.source].name} key={key} />;
+			let source = this.state.sources[entry.source];
+			let show = (source.show && entry.full === source.showFull) ? true : false;
+			let markup = <TLEntry obj={entry} name={source.name} show={show} key={key} />;
 			key ++;
 			return(markup);
 		});
@@ -171,10 +188,37 @@ class Timeline extends React.Component {
 		$(window).resize(throttled);
 	}
 
+	_updateSource(source, full) {
+		console.log('--- _updateSource(' + source + ', ' + full + ')');
+		//console.log(this.state);
+
+		// Toggles the boolean value of a source object's "show" property.
+		let that = this;
+		let newObj = _.clone(this.state.sources);
+
+
+		if (full !== undefined) {
+			// Deciding whether to show full or regular.
+			newObj[source].showFull = !newObj[source].showFull;
+			console.log('newObj[source].showFull: ' + newObj[source].showFull);
+			console.log(' --- ');
+		} else {
+			// Deciding whether to show at all
+			newObj[source].show = !newObj[source].show;
+		}
+		this.setState({
+			sources: newObj
+		});
+		console.log('* this.state.sources[source].showFull: ' + this.state.sources[source].showFull);
+		console.log(that.state);
+		console.log(' --- ');
+	}
+
 	_alternateFloats() {
-		/* Clear out the right-floats and reset them on every other visible timeline event. */
+		// Clear out the right-floats and reset them on every other visible timeline event.
+
 		$('.tl-entry:visible').each(function (i) {
-			/* Test for i+1 because i will start as zero. */
+			// Test for i+1 because i will start as zero.
 			if ( (i+1) % 2 === 0) {
 				$(this).addClass('tl-inverted');
 			} else {
@@ -187,15 +231,14 @@ class Timeline extends React.Component {
 	}
 
 
-
 	render() {
-		let markup = null, sources = this.state.sources;
+		let markup = null;
 
-		if (sources) {
+		if (this.state.initialized) {
 			const entries = this._getEntries();
 			markup = 
 				<div id="timeline">
-					<TLToggles sources={this.state.sources} />
+					<TLToggles sources={this.state.sources} updateSource={this._updateSource.bind(this)} />
 					<ul id="tl-list" className="tl-list UFP KLE RSA TRI ORC RFW FYW ST3 ST4 SFI ITA">
 						{entries}
 					</ul>
@@ -213,14 +256,26 @@ class Timeline extends React.Component {
 	}
 }
 class ButtonGroup extends React.Component {
+	_handleToggle() {
+		console.log('_handleToggle()');
+		this.props.updateSource(this.props.source);
+		console.log(this.props);
+	}
+	_handleFull() {
+		console.log('_handleFull()');
+		this.props.updateSource(this.props.source, this.props.showFull);
+		console.log(this.props);
+	}
 	render() {
+		let activeShow = (this.props.show) ? ' active' : '';
+		let activeFull = (this.props.showFull) ? ' active' : '';
 		let btnFull;
-		if (this.props.full) {
-			btnFull = <button id={this.props.source + "-full"} type="button" className={"btn btn-" + this.props.type + " " + this.props.source}>Full</button>
+		if (this.props.hasFull) {
+			btnFull = <button onClick={this._handleFull.bind(this)} id={this.props.source + "-full"} type="button" className={"btn btn-" + this.props.type + " " + this.props.source + activeFull}>Full</button>
 		}
 		return(
 				<div className="btn-group" role="group" aria-label={this.props.source}>
-					<button id={this.props.source + "-toggle"} type="button" className={"btn btn-" + this.props.type + " active " + this.props.source}>{this.props.source}</button>
+					<button onClick={this._handleToggle.bind(this)} id={this.props.source + "-toggle"} type="button" className={"btn btn-" + this.props.type + " " + this.props.source + activeShow}>{this.props.source}</button>
 					{btnFull}
 				</div>
 			);
@@ -234,10 +289,10 @@ class TLToggles extends React.Component {
 						<div className="panel-body bg-info">
 							<p>Major Governments</p>
 							<div id="source-toolbar" className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-								<ButtonGroup source="UFP" type="primary" full={this.props.sources.UFP.hasFull} />
-								<ButtonGroup source="KLE" type="primary" full={this.props.sources.KLE.hasFull} />
-								<ButtonGroup source="RSA" type="primary" full={this.props.sources.RSA.hasFull} />
-								<ButtonGroup source="ORC" type="primary" full={this.props.sources.ORC.hasFull} />
+								<ButtonGroup source="UFP" type="primary" hasFull={this.props.sources.UFP.hasFull} show={this.props.sources.UFP.show} showFull={this.props.sources.UFP.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="KLE" type="primary" hasFull={this.props.sources.KLE.hasFull} show={this.props.sources.KLE.show} showFull={this.props.sources.KLE.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="RSA" type="primary" hasFull={this.props.sources.RSA.hasFull} show={this.props.sources.RSA.show} showFull={this.props.sources.RSA.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="ORC" type="primary" hasFull={this.props.sources.ORC.hasFull} show={this.props.sources.ORC.show} showFull={this.props.sources.ORC.showFull} updateSource={this.props.updateSource}/>
 							</div>
 							<div>
 								<span className="label label-primary">{"UFP: " + this.props.sources.UFP.name}</span>
@@ -251,11 +306,11 @@ class TLToggles extends React.Component {
 						<div className="panel-body bg-warning">
 							<p>Misc</p>
 							<div id="source-toolbar" className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-								<ButtonGroup source="TRI" type="warning" full={this.props.sources.TRI.hasFull} />
-								<ButtonGroup source="SFI" type="warning" full={this.props.sources.SFI.hasFull} />
-								<ButtonGroup source="ITA" type="warning" full={this.props.sources.ITA.hasFull} />
-								<ButtonGroup source="ST3" type="warning" full={this.props.sources.ST3.hasFull} />
-								<ButtonGroup source="ST4" type="warning" full={this.props.sources.ST4.hasFull} />
+								<ButtonGroup source="TRI" type="warning" hasFull={this.props.sources.TRI.hasFull} show={this.props.sources.TRI.show} showFull={this.props.sources.TRI.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="SFI" type="warning" hasFull={this.props.sources.SFI.hasFull} show={this.props.sources.SFI.show} showFull={this.props.sources.SFI.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="ITA" type="warning" hasFull={this.props.sources.ITA.hasFull} show={this.props.sources.ITA.show} showFull={this.props.sources.ITA.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="ST3" type="warning" hasFull={this.props.sources.ST3.hasFull} show={this.props.sources.ST3.show} showFull={this.props.sources.ST3.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="ST4" type="warning" hasFull={this.props.sources.ST4.hasFull} show={this.props.sources.ST4.show} showFull={this.props.sources.ST4.showFull} updateSource={this.props.updateSource}/>
 							</div>
 							<div>
 								<span className="label label-warning">{"TRI: " + this.props.sources.TRI.name}</span>
@@ -270,8 +325,8 @@ class TLToggles extends React.Component {
 						<div className="panel-body bg-danger">
 							<p>Wars</p>
 							<div id="source-toolbar" className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-								<ButtonGroup source="RFW" type="danger" full={this.props.sources.RFW.hasFull} />
-								<ButtonGroup source="FYW" type="danger" full={this.props.sources.FYW.hasFull} />
+								<ButtonGroup source="RFW" type="danger" hasFull={this.props.sources.RFW.hasFull} show={this.props.sources.RFW.show} showFull={this.props.sources.RFW.showFull} updateSource={this.props.updateSource}/>
+								<ButtonGroup source="FYW" type="danger" hasFull={this.props.sources.FYW.hasFull} show={this.props.sources.FYW.show} showFull={this.props.sources.FYW.showFull} updateSource={this.props.updateSource}/>
 							</div>
 							<div>
 								<span className="label label-danger">{"RFW: " + this.props.sources.RFW.name}</span>
@@ -286,11 +341,13 @@ class TLToggles extends React.Component {
 class TLEntry extends React.Component {
 	render() {
 		let entry = this.props.obj
-		let showHide = (entry.show)  ? ' show' : ' hidden' ;
+		//let showHide = (entry.show)  ? ' show' : ' hidden' ;
+		let show = (this.props.show)  ? ' show' : ' hidden' ;
+		let inverted = (entry.inverted)  ? ' tl-inverted' : '' ;
 
 		return(
 			//<li>{this.props.obj.century}</li>
-			<li id={"tl-entry-" + entry.key} className={"tl-entry " + entry.source + " tl-full-" + entry.full + showHide} data-century={entry.century}>
+			<li id={"tl-entry-" + entry.key} className={"tl-entry " + entry.source + " tl-full-" + entry.full + show + inverted} data-century={entry.century}>
 				<div className="tl-badge"></div>
 				<div className="tl-panel">
 					<div className="tl-heading">
